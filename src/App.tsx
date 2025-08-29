9f73b911bd706de324fa06c6e59b7d0c877d940e
tkiros <Terrykiros@gmail.com>
2025-08-29 06:45:57 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Dashboard from './components/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
